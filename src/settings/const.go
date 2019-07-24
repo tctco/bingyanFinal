52ae3b9e43e4952077e9d9f67fd6b4ab3a7d08c1
tctco <tctco001@163.com>
2019-07-24 16:16:36 +0800

    not final again

--- src/settings/const.go
+++ src/settings/const.go
@@ -4,5 +4,4 @@ const ImagePrefixUrl = "http://127.0.0.1:8080"
 const ImageSavePath = "/upload/"
 const ImageLocalPath = "C:/Users/TC/go/src/onlineShopping/upload"
 
-const DataBasePath = "root:@/onlineshopping?charset=utf8"
-//root:123456@tcp(mysql:3306)/onlineshopping?charset=utf8
+const DataBasePath = "root:123456@tcp(mysql:3306)/onlineshopping?charset=utf8"
